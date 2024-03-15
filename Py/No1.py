1945ae80d9d7e3e45625f9f02419a75a19c3ed90
Gusti Gratia <gustigratia706@gmail.com>
2024-03-15 21:18:42 +0700

    Update

---
+++ Py/No1.py
@@ -0,0 +1,14 @@
+spacing = "="
+print(15*spacing+"DATA MAHASISWA"+15*spacing)
+nama = input("Nama\t\t: ")
+nim = input("NIM\t\t: ")
+asal = input("Asal Kampus\t: ")
+ipk = input("IPK\t\t: ")
+
+print("\n")
+print(15*spacing+"KONFIRMASI REGISTRASI"+15*spacing)
+print("Nama\t\t: " + nama)
+print("NIM\t\t: " + nim)
+print("Asal Kampus\t: " + asal)
+print("IPK\t\t: " + ipk)
+print(nama + " Kamu berhasil melakukan registrasi, silahkan menunggu pengumuman selanjutnya")
